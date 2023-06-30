2bd57aad1de2f27735c746e07340b20c60527c65
bqh123 <131932629+bqh123@users.noreply.github.com>
2023-06-30 18:57:21 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,6 +1,5 @@
-# 在该文件NeuralNetwork类中定义你的模型
+# 在该文件NeuralNetwork类中定义你的模型 
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
-
 import os
 
 os.system("sudo pip3 install torch")
@@ -8,13 +7,16 @@ os.system("sudo pip3 install torchvision")
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torchvision
+import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 # 3x3 卷积定义
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels, out_channels, kernel_size=3,
                      stride=stride, padding=1, bias=False)
+
 # Resnet 的残差块
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
@@ -25,7 +27,7 @@ class ResidualBlock(nn.Module):
         self.conv2 = conv3x3(out_channels, out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
-        
+
     def forward(self, x):
         residual = x
         out = self.conv1(x)
@@ -38,9 +40,9 @@ class ResidualBlock(nn.Module):
         out += residual
         out = self.relu(out)
         return out
-    
+
+# ResNet定义
 class NeuralNetwork(nn.Module):
-    # 使用ResNet
     def __init__(self, block, layers, num_classes=10):
         super(NeuralNetwork, self).__init__()
         self.in_channels = 16
@@ -52,7 +54,7 @@ class NeuralNetwork(nn.Module):
         self.layer3 = self.make_layer(block, 64, layers[2], 2)
         self.avg_pool = nn.AvgPool2d(8)
         self.fc = nn.Linear(64, num_classes)
-        
+
     def make_layer(self, block, out_channels, blocks, stride=1):
         downsample = None
         if (stride != 1) or (self.in_channels != out_channels):
@@ -65,7 +67,7 @@ class NeuralNetwork(nn.Module):
         for i in range(1, blocks):
             layers.append(block(out_channels, out_channels))
         return nn.Sequential(*layers)
-    
+
     def forward(self, x):
         out = self.conv(x)
         out = self.bn(out)
@@ -78,8 +80,12 @@ class NeuralNetwork(nn.Module):
         out = self.fc(out)
         return out
 
-def read_data():
 
+
+
+def read_data():
+    # 这里可自行修改数据预处理，batch大小也可自行调整
+    # 保持本地训练的数据读取和这里一致
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
@@ -90,6 +96,5 @@ def main():
     model = NeuralNetwork(ResidualBlock, [2, 2, 2]) # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.load(parent_dir+'/pth/model.pth', map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
-    
